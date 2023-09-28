790532189a1bcc97376b5811f3ba1e1aa4e807b0
Quoc Dat <quocdat.381872@gmail.com>
2023-09-28 15:32:47 +0700

    :sparkles: update

--- modules/nvim/lua/nihil/plugins/gitsigns.lua
+++ modules/nvim/lua/nihil/plugins/gitsigns.lua
@@ -1,1 +1,14 @@
-return { 'lewis6991/gitsigns.nvim', config = true }
+return {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+        signs = {
+            add = { text = '│' },
+            change = { text = '│' },
+            delete = { text = '_' },
+            topdelete = { text = '‾' },
+            changedelete = { text = '~' },
+            untracked = { text = '│' },
+        },
+        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    },
+}
