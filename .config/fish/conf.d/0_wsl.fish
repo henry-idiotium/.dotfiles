32fd1b487d8264311899ba2c39db861a82517e6f
Henry <quocdat.381872@gmail.com>
2024-05-26 10:47:45 +0700

    :sparkles: update fish

--- .config/fish/conf.d/0_wsl.fish
+++ .config/fish/conf.d/0_wsl.fish
@@ -3,4 +3,13 @@ not string match -q 'Linux*WSL*' -- (uname -sr); and return
 set -gx CLIPBOARD (command -vq win32yank.exe && echo win32yank.exe -i || echo clip.exe)
 
 alias pwsh 'pwsh.exe -WorkingDirectory "~"'
-alias ofe 'xdg-open .'
+
+function ofe -d "Open Windows File Explorer."
+    set -f path $argv
+    [ -z "$path" ] && set path .
+
+    xdg-open "$argv"
+end
+
+set -gx WINDOWS_HOME (cmd.exe /c '<nul set /p=%UserProfile%' 2>/dev/null)
+set WINDOWS_HOME (wslpath -u $WINDOWS_HOME) # convert to linux path
